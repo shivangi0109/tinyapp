1930fd72fc0f7a5e340fda0bcc38139a4bcdf5a5
Shivangi Srivastava <shi06dec@gmail.com>
2023-06-21 19:33:57 +0000

    Add comments

--- express_server.js
+++ express_server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const app = express();
+const express = require("express"); // Import the express library
+const app = express(); // Define our app as an instance of express
 const PORT = 8080; // default port 8080
 
 const urlDatabase = {
@@ -7,14 +7,17 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// Add root route /
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// Add route /urls.json to see a JSON string
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// Add route /hello to send HTMl
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
