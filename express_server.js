ffca1ffc91e98a4449708e122045e00cf435b8dc
Shivangi Srivastava <shi06dec@gmail.com>
2023-06-24 20:01:27 +0000

    Pass in the username to all views that include the _header.ejs partial and modify the _header.ejs partial to display the passed-in username next to the form

--- express_server.js
+++ express_server.js
@@ -1,4 +1,6 @@
 const express = require("express"); // Import the express library
+const cookieParser = require("cookie-parser"); // Import the cookie-parser library
+
 const app = express(); // Define our app as an instance of express
 const PORT = 8080; // default port 8080
 
@@ -11,6 +13,8 @@ app.set("view engine", "ejs");
  * To make this data readable, we will need to use another piece of middleware which will translate, or parse the body.
  */
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -51,18 +55,22 @@ app.get("/hello", (req, res) => {
 
 // Add route /urls to send data to urls_index.ejs
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies.username,
+    urls: urlDatabase 
+  };
   res.render("urls_index", templateVars);
 });
 
 // Add route /urls/new to send data to urls_new.ejs
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies.username };
+  res.render("urls_new", templateVars);
 });
 
 // Add route /urls/:id to send data to urls_show.ejs
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: 'http://www.lighthouselabs.ca' };
+  const templateVars = { username: req.cookies.username, id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
